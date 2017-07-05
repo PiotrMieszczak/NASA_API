05fe39915865d934abbe80fc48a483fe3aaa8e75
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-05 09:08:34 +0200

    created star class

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -5,5 +5,26 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-ctx.arc(100,100,10,0,Math.PI*2);
-ctx.fill();
+
+class Star{
+    constructor(x,y,dx,dy,radius){
+        this.x = x;
+        this.dx = dx; //velocity horizonal
+        
+        this.y = y;
+        this.dy = dy //velocity vertical
+
+        this.radius = radius;
+    }
+    draw(){ //draw 'star' on canvas
+        ctx.beginPath()
+        ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
+        ctx.stroke();
+    }
+    move(){ //update x/y possition on new framerate 
+        this.x += this.dx;
+        this.y += this.dy;
+        this.draw()
+    }
+}
+
