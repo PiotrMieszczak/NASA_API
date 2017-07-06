89bff57996547ef3bd1854f5c3712a7395c30fb5
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-06 14:31:28 +0200

    spinner visual updates

--- src/js/Spinner.jsx
+++ src/js/Spinner.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 
 class Spinner extends React.Component{
     render(){
-        return <div className="main_spinner">
+        return <section id='spinnerSection'>
+                <div className="main_spinner">
                         <div className="dot1"></div>
                         <div className="dot2"></div>
                 </div>
+            </section>
     }
 
 }
