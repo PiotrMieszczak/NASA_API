ed8ba51f51df212fc1b3c6b31f5d74ab47ae334d
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-06 08:05:15 +0200

    visual updates

--- src/js/star.js
+++ src/js/star.js
@@ -1,12 +1,30 @@
 import {ctx} from './canvas_setup.js';
 
-var colorArray = [
+const colorArray = [
     "#78BF82",
     "#A4D17C",
     "#CFD96C",
     "#EBD464",
     "#FFD970",
 ];
+const colorStrokeArray = [
+    "#E7E9E7",
+    "#90057E",
+    "#D0D0DA",
+    "#FFD970",
+    "#F2EDF1"
+];
+
+const maxRadius = 4;
+const minRadius = 2;
+let mouse={
+    x: undefined,
+    y: undefined,
+}
+window.addEventListener('mousemove', (e)=>{ //getting mouse coordinates
+    mouse.x = e.x;
+    mouse.y = e.y
+})
 class Star{
     constructor(x,y,dx,dy,radius){
         this.x = x;
@@ -21,37 +39,44 @@ class Star{
         ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,false)
         ctx.fillStyle= this.color;
         ctx.fill();
+        ctx.strokeStyle = this.styleColor;
+        ctx.stroke();
+       
     }
     move(){ //update x/y possition on new framerate 
-
         if(this.x > window.innerWidth || this.x < 0){
             this.dx = -this.dx
         }
-        
         this.x += this.dx;
         if(this.y > window.innerHeight || this.y < 0){
             this.dx = -this.dx
         }
         this.y += this.dy/2;
-        
+         if(mouse.x - this.x < 30 && mouse.x - this.x > -30
+         && mouse.y - this.y < 30 && mouse.y - this.y > -30){
+            if(this.radius < maxRadius){
+                this.radius += 0.5;}
+            }else if(this.radius > minRadius){
+            this.radius -= 0.5;
+        }
         this.draw()
     }
 }
 function CreateStars(number){
     let StarArray = [];
-    
     for(let i =0; i < number;i++){
-        let radius = Math.random()*10+1;
-
+        let radius = Math.random()*2;
         let x = Math.random()*(window.innerWidth-radius*2)+radius;
         let y = Math.random()*(window.innerHeight-radius*2)+radius;
         let dx = (Math.random()-0.5)*0.8;
         let dy = (Math.random()-0.5)*0.8;
-        
         StarArray.push(new Star(x,y,dx,dy,radius))
     }
     return StarArray; 
 }   
-let stars = CreateStars(250);
 
+let stars = CreateStars(1500);
 export {stars};
+
+
+
