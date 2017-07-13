18c550bc95a7c055a221f302962d430b5a5c4dc8
PiotrMieszczak <mieszczak.piotr@gmail.com>
2017-07-13 22:45:04 +0200

    comment

--- src/js/Iss.jsx
+++ src/js/Iss.jsx
@@ -5,7 +5,7 @@ class ISS extends React.Component{
         console.log('test');
         return <section id="home" >
                 <iframe 
-                src={"https://www.youtube.com/embed/RtU_mdL2vBM"} 
+                src={"https://www.youtube.com/embed/RtU_mdL2vBM"} //embed endpoint allows outside requests, /watch doesn't
                 frameBorder={"0"}
                 scrolling={"no"}
                 allowFullScreen
